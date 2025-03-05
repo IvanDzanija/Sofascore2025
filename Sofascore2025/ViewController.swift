49b3eeb1009c83a2856fcca96784ee02627cc1c4
Dzanija Ivan <ivan.dzanija222@gmail.com>
2025-03-05 10:49:48 +0100

    Added SnapKit

---
+++ Sofascore2025/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sofascore2025
+//
+//  Created by Ivan Dzanija on 03.03.2025..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
